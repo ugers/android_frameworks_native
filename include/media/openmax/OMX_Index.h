d77ca55a253b65bf7e5a1290beca77cd5158634a
ugers <yugers@gmail.com>
2013-10-07 11:24:54 +0000

    Update OMX_Index.h

--- include/media/openmax/OMX_Index.h
+++ include/media/openmax/OMX_Index.h
@@ -264,6 +264,13 @@ typedef enum OMX_INDEXTYPE {
        private indexes are not guaranteed unique and therefore should
        only be sent to the appropriate component. */
 
+    OMX_IndexParamEnableAndroidBuffers    = 0x7FF00000,
+  OMX_IndexParamGetAndroidNativeBuffer,
+  OMX_IndexParamUseAndroidNativeBuffer,
+  OMX_IndexParamUseAndroidNativeBuffer2,
+
+  OMX_IndexParamVendorFlushBuffer,
+
     OMX_IndexMax = 0x7FFFFFFF
 
 } OMX_INDEXTYPE;
